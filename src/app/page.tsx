5a069eaa546b1e229d25f3359e4fe195c04bad24
Khoa Tran <tnakhoa2311@gmail.com>
2025-12-28 23:04:26 -0500

    add: about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AboutView from "@/views/about/AboutView";
 import HeroSection from "@/views/home/HeroSection";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <AboutView />
     </>  
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface HeaderProps {
+    title: string;
+    as?: "h1" | "h2"; 
+}
+
+export default function Header({ title, as }: HeaderProps) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
