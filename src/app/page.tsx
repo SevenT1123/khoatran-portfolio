ffa59dd4212bc81284d02e380254ecf71c236c68
Khoa Tran <tnakhoa2311@gmail.com>
2025-12-31 12:17:05 -0500

    add: nav footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import NavFooter from "@/components/NavFooter";
 import HeroSection from "@/views/home/HeroSection";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <NavFooter />
     </>  
   );
 }
